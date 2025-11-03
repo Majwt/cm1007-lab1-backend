43885627d86ed010b2519530fe17c6ad782ced92
BaronBalt <walmgren@kth.se>
2025-11-03 11:45:28 +0100

    Managed to transfer Patient from backend to frontend

---
+++ src/main/java/web/backend/Model/Patient.java
@@ -0,0 +1,39 @@
+package web.backend.Model;
+
+public class Patient {
+    private int id;
+    private String name;
+    private String birthday;
+
+    public Patient() {}
+
+    public Patient(int id, String name, String birthday) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+}
--- src/main/java/web/backend/TestController.java
+++ src/main/java/web/backend/TestController.java
@@ -3,13 +3,23 @@ package web.backend;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import web.backend.Model.Patient;
 
 @RestController
 public class TestController {
 
-    @CrossOrigin(origins = {"http://localhost:80", "http://localhost:5173"})
     @GetMapping("/test")
+    @CrossOrigin(origins = {"http://localhost:80", "http://localhost:5173"})
     public String testEndpoint() {
         return "Test endpoint is working!";
+    }
+
+    @GetMapping("/patients")
+    @CrossOrigin(origins = {"http://localhost:80", "http://localhost:5173"})
+    public Patient[] getPatients() {
+        Patient p1 = new Patient(1, "Walter Almgren", "1932-04-12");
+        Patient p2 = new Patient(2, "Theodor Wase", "2037-12-24");
+
+        return new Patient[]{p1, p2};
     }
 }
